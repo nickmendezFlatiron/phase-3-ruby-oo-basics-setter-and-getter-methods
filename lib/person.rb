d3e135c4a7d4679a78044dd885faadaf42db8b66
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-05-15 15:41:56 -0400

    completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,25 @@
+require 'pry'
 class Person
 
+#   attr_accessor :name
+#   attr_accessor :job
+#   def initialize(name , job)
+#     @name = name
+#     @job = job
+#   end
+# binding.pry
+
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+
+  def job=(job)
+    @job = job
+  end
+  def job
+    @job
+  end
 end
